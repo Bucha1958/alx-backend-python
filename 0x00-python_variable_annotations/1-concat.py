7c47847ee9365dc9623a860de40cecf26d4ad488
Bucha1958 <okoriemmadu@gmail.com>
2023-08-05 13:04:20 -0700

    new function added

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""
+concat module
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+        Function concat takes two parameters
+        str1 first parameter
+        str2 second parameter
+        Return string
+    """
+    return str1 + str2
