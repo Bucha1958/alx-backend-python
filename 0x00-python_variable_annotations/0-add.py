3467d1522c8fad94d5c5f6ec8fe4b899283b6055
Bucha1958 <okoriemmadu@gmail.com>
2023-08-05 11:56:54 -0700

    added a function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
+
 """
     Add function that takes two parameters
     a - float
     b - float
     Return float
 """
+
+
 def add(a: float, b: float) -> float:
     return a + b
