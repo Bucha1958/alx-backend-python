528cc5fd4712ced60bbebaa77a8d85ec39aed6cf
Bucha1958 <okoriemmadu@gmail.com>
2023-08-05 12:02:07 -0700

    commented my function

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -9,4 +9,14 @@
 
 
 def add(a: float, b: float) -> float:
+    """
+        Add two float numbers and return the result.
+
+        Parameters:
+        a (float): The first input float number.
+        b (float): The second input float number.
+
+        Returns:
+        float: The sum of the two input numbers.
+    """
     return a + b
