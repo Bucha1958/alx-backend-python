2aeb5267fcd4188f8f4ec3586b7534dfeabbe10e
Bucha1958 <okoriemmadu@gmail.com>
2023-08-05 13:32:23 -0700

    Added

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+import math
+"""
+    A module 2-floor
+"""
+
+
+def floor(n: float) -> float:
+    """
+        Floor function takes a float type as argument
+        n parameter - float
+        Return float that is been converted to floor
+    """
+    return math.floor(n)
