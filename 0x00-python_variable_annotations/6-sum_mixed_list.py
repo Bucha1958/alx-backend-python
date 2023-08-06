c87db11c4a2c088e634a9af5402eb6aeeeb56e11
Bucha1958 <okoriemmadu@gmail.com>
2023-08-06 07:52:10 -0700

    added

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+from typing import List
+"""6-sum_mixed_list module"""
+Vector = List[float]
+
+
+def sum_mixed_list(mxd_lst: Vector) -> float:
+    """
+    sum_mixed_list function takes list as argument
+    """
+    return sum(mxd_lst)
