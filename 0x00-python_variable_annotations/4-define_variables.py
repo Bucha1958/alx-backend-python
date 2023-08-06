18c989b4bb3c689f1e0c4b3c496498b80623f458
Bucha1958 <okoriemmadu@gmail.com>
2023-08-06 07:26:15 -0700

    Added a function

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 # define_variables module
 
+# variable a
 a: int = 1
+# variable pi
 pi: float = 3.14
+# variable i_understand_annotations
 i_understand_annotations: bool = True
+# variable school
 school: str = 'Holberton'
---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" 5-sum_list for module"""
+from typing import List
+
+Vector = List[float]
+
+
+def sum_list(input_list: Vector) -> float:
+    """
+        sum_list function passes list of float as a parameter
+        Returns a float
+    """
+    return sum(input_list)
