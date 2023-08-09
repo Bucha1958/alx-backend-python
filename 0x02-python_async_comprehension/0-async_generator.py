188e3f7e43036a69db6d1f2d4aa9ba84e4ca8562
Bucha1958 <okoriemmadu@gmail.com>
2023-08-08 17:50:41 -0700

    async comprehension

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -13,7 +13,7 @@ async def async_generator() -> Generator[float, None, None]:
     """
     i = 0
     while i < 10:
-        randint = random.randrange(1, 11)
+        randint = random.uniform(0, 10)
         await asyncio.sleep(1)
         yield randint
         i += 1
